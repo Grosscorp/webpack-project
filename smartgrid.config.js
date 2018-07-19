a0452278a0abbea3d8d270a370ae132b92e84cb1
Mykola <mgrosscorp@gmail.com>
2018-07-19 17:03:17 +0300

    add smart-grid

---
+++ smartgrid.config.js
@@ -0,0 +1,41 @@
+/*
+For creating grid run 
+'node smartgrid.config.js'
+in terminal
+*/
+
+const smartgrid = require('smart-grid');
+
+const settings = {
+  filename: 'smartgrid',
+  outputStyle: 'scss',
+  columns: 12, 
+  offset: '30px', 
+  mobileFirst: false, 
+  container: {
+    maxWidth: '1280px',
+    fields: '30px'
+  },
+  breakPoints: {
+    lg: {
+      width: '1200px'
+    },
+    md: {
+      width: '992px',
+      fields: '15px'
+    },
+    sm: {
+      width: '720px'
+    },
+    xs: {
+      width: '576px'
+    }
+  },
+  mixinNames: {
+    container: 'wrap',
+    row: 'row'
+  },
+  tab: '  ',
+};
+
+smartgrid('./src/scss', settings);
